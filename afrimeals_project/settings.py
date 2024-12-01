6526549d5bfd518d741a4a8214d1d0ac45c0cfb3
Mr. Managwu <39161025+chidoziemanagwu@users.noreply.github.com>
2024-12-01 20:55:58 +0000

    updates

--- afrimeals_project/settings.py
+++ afrimeals_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com','*']
 
 # Application definition
 INSTALLED_APPS = [
