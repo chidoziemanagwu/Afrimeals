458b4ecafaa2780b3c37c2a9af2a25c8fb193252
Mr. Managwu <39161025+chidoziemanagwu@users.noreply.github.com>
2024-12-03 05:46:45 +0000

    updates

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-# exit on error
+# build.sh
+
+# Make script exit on first error
 set -o errexit
 
+# Install Python dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
