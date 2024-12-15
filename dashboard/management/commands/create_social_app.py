1c712ab4635e9a672d27b624fa0bbd28edd1be62
Mr. Managwu <39161025+chidoziemanagwu@users.noreply.github.com>
2024-12-15 13:02:21 +0000

    .

--- dashboard/management/commands/create_social_app.py
+++ dashboard/management/commands/create_social_app.py
@@ -1,9 +1,9 @@
 # dashboard/management/commands/create_social_app.py  
 
+import os  
 from django.core.management.base import BaseCommand  
 from allauth.socialaccount.models import SocialApp  
 from django.contrib.sites.models import Site  
-import os  
 
 class Command(BaseCommand):  
     help = 'Create Google social application if it does not exist'  
@@ -12,13 +12,21 @@ class Command(BaseCommand):
         site, created = Site.objects.get_or_create(domain='afrimeals.onrender.com', name='Afrimeals')  
 
         if not SocialApp.objects.filter(provider='google').exists():  
+            print("Creating Google social application...")  
+
+            print("Google Client ID:", os.getenv('GOOGLE_CLIENT_ID'))  
+            print("Google Client Secret:", os.getenv('GOOGLE_CLIENT_SECRET'))  
             app = SocialApp.objects.create(  
                 provider='google',  
                 name='Google',  
-                client_id=os.getenv('GOOGLE_CLIENT_ID'),  # Use environment variable  
-                secret=os.getenv('GOOGLE_CLIENT_SECRET'),  # Use environment variable  
+                client_id=os.getenv('GOOGLE_CLIENT_ID'),  
+                secret=os.getenv('GOOGLE_CLIENT_SECRET'),  
             )  
+
+
+            print("2. Google Client ID:", os.getenv('GOOGLE_CLIENT_ID'))  
+            print("2. Google Client Secret:", os.getenv('GOOGLE_CLIENT_SECRET'))  
             app.sites.add(site)  
-            self.stdout.write(self.style.SUCCESS('Successfully created Google social application.'))  
+            print("Google social application created successfully.")   
         else:  
             self.stdout.write(self.style.WARNING('Google social application already exists.'))  
